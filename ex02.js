28b74005dd32269c1162c74fcb080b630b1071a4
stepanenko <serhiy.stepanenko@aiesec.net>
2020-06-13 10:32:26 +0300

    added solution 2

--- ex02.js
+++ ex02.js
@@ -1,5 +1,5 @@
 
-const reviews = [4.5, 4.0, 5.0, 1.0, 1.0, 5.0, 3.0, 4.0, 1.0, 5.0, 4.5];
+const reviews = [4.5, 4.0, 5.0, 5.0, 3.0, 4.0, 5.0, 4.5];
 
 function getReviews(acc, review) {
   review = review.toFixed(1);
@@ -8,5 +8,14 @@ function getReviews(acc, review) {
 }
 
 const reviewsCount = reviews.reduce(getReviews, {});
+// console.log(reviewsCount);
 
-console.log(reviewsCount); // { '4.5': 2, '4.0': 2, '5.0': 3, '1.0': 3, '3.0': 1 }
+
+// Moore's answer:
+function groupBy(acc, review) {
+  const count = acc[review] || 0;
+  return { ...acc, [review]: count + 1 };
+}
+
+const countGroupedByReview = reviews.reduce(groupBy, {});
+console.log(countGroupedByReview);
