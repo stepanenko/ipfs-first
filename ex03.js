3ed4513356ca08685fa869a1a294ab8158d4fa6a
stepanenko <serhiy.stepanenko@aiesec.net>
2020-06-14 12:10:27 +0300

    solution with map

--- ex03.js
+++ ex03.js
@@ -7,3 +7,28 @@ const studentGrades = [
   { name: 'Gina', grade: 54 }
 ];
 
+const messages = [
+  'Excellent Job',
+  'Nice Job',
+  'Well done',
+  'What happened',
+  'Not good'
+];
+
+function getFeedback(messages) {
+  return function (student) {
+    if (student.grade >= 90 ) message = messages[0];
+    else if (student.grade >= 80) message = messages[1];
+    else if (student.grade >= 70) message = messages[2];
+    else if (student.grade >= 60) message = messages[3];
+    else message = messages[4];
+    return `${message} ${student.name}, you got ${student.grade}`;
+  }
+}
+
+const joeFeedback = getFeedback(messages)({ name: 'Joe', grade: 33 });
+console.log(joeFeedback);  // Not good Joe, you got 33
+
+const studentsFeedback = studentGrades.map(getFeedback(messages));
+console.log(studentsFeedback);
+
